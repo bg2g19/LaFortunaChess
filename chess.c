c2a98f82b27ca3ea7d2df4bc99a329ea96875e0a
bg2g19 <56226365+bg2g19@users.noreply.github.com>
2021-05-08 22:31:41 +0100

    Update chess.c

--- chess.c
+++ chess.c
@@ -1,3 +1,8 @@
+/*  Author: Ben Gibbs
+ * Licence: This work is licensed under the Creative Commons Attribution License.
+ *           View this license at http://creativecommons.org/about/licenses/
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
